4bc8ea43600966d3e06c00a5862d6e84cad7551a
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-09-15 00:26:40 +0400

    create project fundamentals

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+        dbName: 'notificationsDb',
+      }),
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { HttpExceptionFilter } from './exception-filters/http.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 7000);
+  const PORT = Number(process.env.PORT);
+  const NODE_ENV = String(process.env.NODE_ENV);
+
+  const tcpApp = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: 'notifications-service',
+        port: PORT,
+      },
+    },
+  );
+
+  // Filter
+  tcpApp.useGlobalFilters(new HttpExceptionFilter());
+
+  await tcpApp.listen();
+
+  console.log(`✅ NODE_ENV => `, NODE_ENV);
+  console.log(`✅ Server is working on ${PORT} port`);
 }
 bootstrap();
